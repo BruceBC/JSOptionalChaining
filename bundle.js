0294038c43115802959c9670293e04a0136c0d4f
Bruce Colby <brucecolby@bruces-macbook.lan>
2018-02-26 20:02:33 -0500

    Initial Commit

---
+++ bundle.js
@@ -0,0 +1,7 @@
+var person = {
+  name: 'John Doe',
+  age: 24
+};
+console.log("NAME: ".concat(person === null || person === void 0 ? void 0 : person.name));
+console.log("AGE: ".concat(person === null || person === void 0 ? void 0 : person.age));
+console.log("GENDER: ".concat(person === null || person === void 0 ? void 0 : person.gender));
---
+++ index.js
@@ -0,0 +1,8 @@
+const person = {
+    name: 'John Doe',
+    age: 24
+}
+
+console.log(`NAME: ${person?.name}`)
+console.log(`AGE: ${person?.age}`)
+console.log(`GENDER: ${person?.gender}`)
